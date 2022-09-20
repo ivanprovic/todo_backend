3c103ed05eba06d0e1678a17776726536464ac66
Ivan Provic <ivan.pik82@gmail.com>
2022-09-20 22:25:55 +0200

    feat: add permissions

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 
 from .models import Post
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 
 
@@ -10,5 +11,6 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
