8a909a0c4e53f342339591316ef62eeceea17f94
Ivan Provic <ivan.pik82@gmail.com>
2022-10-15 09:55:20 +0200

    feat: add user serializer

--- todos/serializers.py
+++ todos/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Post
 
@@ -7,3 +8,10 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ('id', 'author', 'title', 'body', 'created_at',)
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username')
--- todos/urls.py
+++ todos/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import PostList, PostDetail
+from .views import UserList, UserDetail, PostList, PostDetail
 
 urlpatterns = [
+    path('users/', UserList.as_view()),
+    path('users/<int:pk>/', UserDetail.as_view()),
     path('<int:pk>/', PostDetail.as_view()),
     path('', PostList.as_view()),
 ]
